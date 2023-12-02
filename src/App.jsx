b5d407c9e1ba51fbe0bbe9da2c013b8c08960e7d
agus <agustinherlein@hotmail.com>
2023-12-02 15:24:36 -0300

    xd

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,30 @@
 
 import './App.css';
 import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import flipis from "./img/flipis.png"
 import Header from "./components/Header"
-import { Fragment } from 'react';
+
 
 function App() {
   return (
     <div className="app">
       <Header/>
-      <div className="main">Lorem ipsum XD</div>
+      <div className="main">
+        <BrowserRouter>
+          <Routes>
+            <Route path='/' element={<div>Lorem ipsum XD</div>}/>
+          </Routes>
+          
+        </BrowserRouter>
+        </div>
     
     </div>
   );
 }
+
+// ReactDOM.render((<BrowserRouter basename={process.env.PUBLIC_URL}>
+//   <App />
+//   <BrowserRouter/>), ...)
 
 export default App;
