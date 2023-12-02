fd170e435b273c3d5c218e6e149e530f30b3b114
agus <agustinherlein@hotmail.com>
2023-12-02 11:48:15 -0300

    add stuff

---
+++ app/page.jsx
@@ -0,0 +1,13 @@
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <main className="flex items-center justify-between h-100">
+      <h1>Hola Flipi</h1>
+      <div>
+
+      </div>
+      <button className="btn btn-primary">Esto es un botón</button>
+    </main>
+  )
+}
